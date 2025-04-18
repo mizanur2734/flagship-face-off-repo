260e1a369eea9f357fc1dbd27c3eb5d41ff68573
mizanff <mrmizanurrahman626@gmail.com>
2025-04-18 06:13:16 -0500

    added the project setup and route component

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl font-bold underline">Vite + React</h1>
+      <button className="btn btn-primary">button</button>
+    </>
+  );
+}
+
+export default App;
