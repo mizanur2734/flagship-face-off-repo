fd1c79f30780b8ab1aa4164244368b4e58d646ae
mizanff <mrmizanurrahman626@gmail.com>
2025-04-19 00:46:23 -0500

    show all button complit

--- src/components/PhoneCard.jsx
+++ src/components/PhoneCard.jsx
@@ -14,7 +14,13 @@ const PhoneCard = ({ phone }) => {
         <p>{description}</p>
         <div className="card-actions justify-end">
           <Link to="/phone-details">
-            <button className="btn btn-primary">View More</button>
+            <button class="relative inline-block px-4 py-2 font-medium group">
+              <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+              <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+              <span class="relative text-black group-hover:text-white">
+                View More
+              </span>
+            </button>
           </Link>
         </div>
       </div>
